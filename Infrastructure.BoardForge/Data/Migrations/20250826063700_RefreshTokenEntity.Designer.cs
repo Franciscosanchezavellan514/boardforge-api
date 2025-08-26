40cb209ad5522dead091861fdeee4add23bbf992
DlopezS98 <01dlopezs98@gmail.com>
2025-08-26 00:40:08 -0600

    Add RefreshToken migration and update DbContext model snapshot

--- Infrastructure.BoardForge/Data/Migrations/20250826063700_RefreshTokenEntity.Designer.cs
+++ Infrastructure.BoardForge/Data/Migrations/20250826063700_RefreshTokenEntity.Designer.cs
@@ -4,6 +4,7 @@ using DevStack.Infrastructure.BoardForge.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DevStack.Infrastructure.BoardForge.Data.Migrations
 {
     [DbContext(typeof(BoardForgeDbContext))]
-    partial class BoardForgeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250826063700_RefreshTokenEntity")]
+    partial class RefreshTokenEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
