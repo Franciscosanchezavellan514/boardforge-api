ea3bc842a341257bfe5d30fae55741bb781eb4bf
DlopezS98 <01dlopezs98@gmail.com>
2025-08-25 22:44:14 -0600

    Add TokenResponseDTO and IAuthenticationService interface

---
+++ Application.Endpoint/DTOs/Response/TokenResponseDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace DevStack.Application.BoardForge.DTOs.Response;
+
+public class TokenResponseDTO
+{
+    public string Token { get; set; } = string.Empty;
+    public DateTime ExpiresIn { get; set; }
+}
---
+++ Application.Endpoint/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,8 @@
+﻿using DevStack.Application.BoardForge.DTOs.Response;
+
+namespace DevStack.Application.BoardForge.Interfaces;
+
+public interface IAuthenticationService
+{
+    Task<TokenResponseDTO> AuthenticateAsync(string email, string password);
+}
