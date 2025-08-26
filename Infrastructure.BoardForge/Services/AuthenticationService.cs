6067ed06f5b52b451422e52a7e3bf666b13d774e
DlopezS98 <01dlopezs98@gmail.com>
2025-08-26 12:15:53 -0600

    Initial authentication service setup

---
+++ Infrastructure.BoardForge/Services/AuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿using DevStack.Application.BoardForge.DTOs.Response;
+using DevStack.Application.BoardForge.Interfaces;
+
+namespace DevStack.Infrastructure.BoardForge.Services;
+
+public class AuthenticationService : IAuthenticationService
+{
+    public Task<TokenResponseDTO> AuthenticateAsync(string email, string password)
+    {
+        if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+            throw new ArgumentException("Email and password must be provided.");
+
+        throw new NotImplementedException();
+    }
+}
