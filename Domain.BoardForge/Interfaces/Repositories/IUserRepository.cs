74733c63e2bcd9653662607e9a83fe9332f559f6
DlopezS98 <01dlopezs98@gmail.com>
2025-08-26 23:32:24 -0600

    Implement method for user retrieval by email

--- Domain.BoardForge/Interfaces/Repositories/IUserRepository.cs
+++ Domain.BoardForge/Interfaces/Repositories/IUserRepository.cs
@@ -2,4 +2,7 @@ using DevStack.Domain.BoardForge.Entities;
 
 namespace DevStack.Domain.BoardForge.Interfaces.Repositories;
 
-public interface IUserRepository : IAsyncRepository<User> { }
+public interface IUserRepository : IAsyncRepository<User>
+{
+    Task<User?> GetByEmailAsync(string email);
+}
--- Infrastructure.BoardForge/Data/Repositories/UserRepository.cs
+++ Infrastructure.BoardForge/Data/Repositories/UserRepository.cs
@@ -1,6 +1,15 @@
 using DevStack.Domain.BoardForge.Entities;
 using DevStack.Domain.BoardForge.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevStack.Infrastructure.BoardForge.Data.Repositories;
 
-public class UserRepository(BoardForgeDbContext dbContext) : EfRepository<User>(dbContext), IUserRepository { /* Add any user-specific methods here */ }
+public class UserRepository(BoardForgeDbContext dbContext) : EfRepository<User>(dbContext), IUserRepository
+{
+    private readonly BoardForgeDbContext _dbContext = dbContext;
+
+    public Task<User?> GetByEmailAsync(string email)
+    {
+        return _dbContext.Users.FirstOrDefaultAsync(u => u.Email == email);
+    }
+}
