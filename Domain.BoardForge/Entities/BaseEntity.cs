4f72b819390c4035e564ee0d2303306d011f9f2b
DlopezS98 <01dlopezs98@gmail.com>
2025-08-27 00:16:07 -0600

    Change user entity inheritance and set default to 'is active' property

--- Domain.BoardForge/Entities/BaseEntity.cs
+++ Domain.BoardForge/Entities/BaseEntity.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace DevStack.Domain.BoardForge.Entities;
 
@@ -6,6 +7,7 @@ public abstract class BaseEntity
 {
     [Key]
     public int Id { get; set; }
+    [DefaultValue(true)]
     public bool IsActive { get; set; }
 }
 
--- Domain.BoardForge/Entities/User.cs
+++ Domain.BoardForge/Entities/User.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace DevStack.Domain.BoardForge.Entities;
 
-public class User : SoftDeletableEntity
+public class User : BaseEntity
 {
     [Required, MaxLength(256)]
     public string Email { get; set; } = string.Empty;
@@ -13,6 +13,9 @@ public class User : SoftDeletableEntity
     [Required, MaxLength(100)]
     public string DisplayName { get; set; } = string.Empty;
     public bool EmailConfirmed { get; set; } = false;
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
 
     // Navigation properties
     public ICollection<TeamMembership> TeamMemberships { get; set; } = [];
