30bedd925d2934e8bd549692c24a6c0c9a7826d2
DlopezS98 <01dlopezs98@gmail.com>
2025-08-27 14:13:27 -0600

    Add error response model

---
+++ BoardForgeAPI/Models/HttpErrorResponse.cs
@@ -0,0 +1,38 @@
+using System.Net;
+using DevStack.Domain.BoardForge.Exceptions;
+
+namespace DevStack.BoardForgeAPI.Models;
+
+public class HttpErrorResponse
+{
+    public int StatusCode { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public List<string> Errors { get; set; } = [];
+
+    public HttpErrorResponse(int statusCode, string message, List<string> errors)
+    {
+        StatusCode = statusCode;
+        Message = message;
+        Errors = errors;
+    }
+
+    public HttpErrorResponse(int statusCode, string message)
+    {
+        StatusCode = statusCode;
+        Message = message;
+    }
+
+
+    public static HttpErrorResponse From(Exception exception)
+    {
+        return exception switch
+        {
+            ApplicationException e => new HttpErrorResponse(StatusCodes.Status400BadRequest, e.Message),
+            UnauthorizedAccessException e => new HttpErrorResponse(StatusCodes.Status401Unauthorized, e.Message),
+            ArgumentException e => new HttpErrorResponse(StatusCodes.Status400BadRequest, e.Message),
+            EntityNotFoundException e => new HttpErrorResponse(StatusCodes.Status404NotFound, e.Message),
+            // Add more exceptions here...
+            _ => new HttpErrorResponse(StatusCodes.Status500InternalServerError, "Internal server error. Please retry later."),
+        };
+    }
+}
