02803860c163145b1443202f7633e43e492a5166
DlopezS98 <01dlopezs98@gmail.com>
2025-08-26 23:07:56 -0600

    update refresh token repository contract

--- Domain.BoardForge/Interfaces/Repositories/IRefreshTokenRepository.cs
+++ Domain.BoardForge/Interfaces/Repositories/IRefreshTokenRepository.cs
@@ -4,7 +4,7 @@ namespace DevStack.Domain.BoardForge.Interfaces.Repositories;
 
 public interface IRefreshTokenRepository
 {
-    Task<RefreshToken> GetByTokenAsync(string token);
+    Task<RefreshToken?> GetByTokenAsync(string token);
     Task AddAsync(RefreshToken refreshToken);
     Task RemoveAsync(string token);
 }
