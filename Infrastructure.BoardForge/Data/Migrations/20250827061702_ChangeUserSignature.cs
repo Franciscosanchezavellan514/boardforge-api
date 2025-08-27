faf86473a236b7f47926d161f6fedf295f2d30f3
DlopezS98 <01dlopezs98@gmail.com>
2025-08-27 00:18:18 -0600

    Generate and apply migrations for user table

---
+++ Infrastructure.BoardForge/Data/Migrations/20250827061702_ChangeUserSignature.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevStack.Infrastructure.BoardForge.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeUserSignature : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedBy",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DeletedBy",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UpdatedBy",
+                table: "Users",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
