c3d12f8f32a32a0f707f1f40c954c336543a4007
DlopezS98 <01dlopezs98@gmail.com>
2025-08-31 16:30:19 -0600

    Register TeamRoleAuthorizationHandler and add authorization policies in Program.cs

--- BoardForgeAPI/Program.cs
+++ BoardForgeAPI/Program.cs
@@ -1,6 +1,8 @@
+using DevStack.BoardForgeAPI.Authorization;
 using DevStack.BoardForgeAPI.Extensions;
 using DevStack.BoardForgeAPI.Middlewares;
 using DevStack.Infrastructure.BoardForge;
+using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,9 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddJwtAuthentication(builder.Configuration);
 builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddAuthorizationPolicies();
+
+builder.Services.AddSingleton<IAuthorizationHandler, TeamRoleAuthorizationHandler>();
 
 var app = builder.Build();
 
