cf0689f6b99137ed5f50c53954d93cb72cd48e74
DlopezS98 <01dlopezs98@gmail.com>
2025-08-31 21:43:53 -0600

    Implement SpecificationEvaluator class for dynamic query generation

---
+++ Infrastructure.BoardForge/Data/SpecificationEvaluator.cs
@@ -0,0 +1,44 @@
+using DevStack.Domain.BoardForge.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevStack.Infrastructure.BoardForge.Data;
+
+public class SpecificationEvaluator<T> where T : class
+{
+    public static IQueryable<T> GetQuery(IQueryable<T> inputQuery, ISpecification<T> specification)
+    {
+        // Apply criteria
+        var query = inputQuery;
+
+        if (specification.Criteria != null)
+        {
+            query = query.Where(specification.Criteria);
+        }
+
+        if (specification.OrderBy != null)
+        {
+            query = query.OrderBy(specification.OrderBy);
+        }
+
+        if (specification.OrderByDescending != null)
+        {
+            query = query.OrderByDescending(specification.OrderByDescending);
+        }
+
+        if (specification.GroupBy != null)
+        {
+            query = query.GroupBy(specification.GroupBy).SelectMany(g => g);
+        }
+
+        if (specification.IsPagingEnabled)
+        {
+            if (specification.Skip.HasValue) query = query.Skip(specification.Skip.Value);
+            if (specification.Take.HasValue) query = query.Take(specification.Take.Value);
+        }
+
+        // Apply includes
+        query = specification.Includes.Aggregate(query, (current, include) => current.Include(include));
+
+        return query;
+    }
+}
