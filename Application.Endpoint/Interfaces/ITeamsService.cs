399bfcb6557f0f3e360f342d048345c5b26e4f9d
DlopezS98 <01dlopezs98@gmail.com>
2025-09-01 10:13:39 -0600

    Implement team management for adding members, creating, and updating teams

--- Application.Endpoint/Interfaces/ITeamsService.cs
+++ Application.Endpoint/Interfaces/ITeamsService.cs
@@ -1,3 +1,4 @@
+using DevStack.Application.BoardForge.DTOs.Request;
 using DevStack.Application.BoardForge.DTOs.Response;
 
 namespace DevStack.Application.BoardForge.Interfaces;
@@ -5,4 +6,7 @@ namespace DevStack.Application.BoardForge.Interfaces;
 public interface ITeamsService
 {
     Task<IEnumerable<TeamResponse>> ListMyTeamsAsync(int userId);
+    Task<TeamResponse> CreateAsync(BaseRequest<CreateTeamRequest> request);
+    Task<TeamResponse> UpdateAsync(BaseRequest<UpdateTeamRequest> request);
+    Task AddMemberAsync(BaseRequest<AddTeamMemberRequest> request);
 }
--- Application.Endpoint/Services/TeamsService.cs
+++ Application.Endpoint/Services/TeamsService.cs
@@ -1,5 +1,8 @@
+using DevStack.Application.BoardForge.DTOs.Request;
 using DevStack.Application.BoardForge.DTOs.Response;
 using DevStack.Application.BoardForge.Interfaces;
+using DevStack.Domain.BoardForge.Entities;
+using DevStack.Domain.BoardForge.Exceptions;
 using DevStack.Domain.BoardForge.Interfaces.Repositories;
 using DevStack.Domain.BoardForge.Specifications;
 
@@ -8,6 +11,51 @@ namespace DevStack.Application.BoardForge.Services;
 public class TeamsService(IUnitOfWork unitOfWork) : ITeamsService
 {
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+    public async Task AddMemberAsync(BaseRequest<AddTeamMemberRequest> request)
+    {
+        if (request.ObjectId == null) throw new ArgumentException("ObjectId must be provided");
+        if (!TeamMembershipRole.AllRoles.Contains(request.Data.Role)) throw new ArgumentException("Invalid role");
+
+        TeamMembership membership = new()
+        {
+            TeamId = request.ObjectId.Value,
+            UserId = request.Data.UserId,
+            Role = request.Data.Role,
+            CreatedAt = DateTime.UtcNow,
+            CreatedBy = request.UserId,
+            IsActive = true,
+        };
+
+        await _unitOfWork.TeamMemberships.AddAsync(membership);
+        await _unitOfWork.SaveChangesAsync();
+    }
+
+    public async Task<TeamResponse> CreateAsync(BaseRequest<CreateTeamRequest> request)
+    {
+        Team newTeam = new()
+        {
+            Name = request.Data.Name,
+            Description = request.Data.Description,
+            CreatedBy = request.UserId,
+            CreatedAt = DateTime.UtcNow
+        };
+        await _unitOfWork.Teams.AddAsync(newTeam);
+
+        TeamMembership membership = new()
+        {
+            Team = newTeam,
+            UserId = request.UserId,
+            Role = TeamMembershipRole.Owner,
+            CreatedAt = DateTime.UtcNow,
+            CreatedBy = request.UserId,
+            IsActive = true
+        };
+        await _unitOfWork.TeamMemberships.AddAsync(membership);
+        await _unitOfWork.SaveChangesAsync();
+
+        return new TeamResponse(newTeam.Id, newTeam.Name, newTeam.Description, newTeam.TeamMemberships.Count);
+    }
 
     public async Task<IEnumerable<TeamResponse>> ListMyTeamsAsync(int userId)
     {
@@ -21,5 +69,22 @@ public class TeamsService(IUnitOfWork unitOfWork) : ITeamsService
             )
         );
         return teamResponses;
+    }
+
+    public async Task<TeamResponse> UpdateAsync(BaseRequest<UpdateTeamRequest> request)
+    {
+        if (request.ObjectId == null) throw new ArgumentException("ObjectId must be provided");
+
+        Team? dbTeam = await _unitOfWork.Teams.GetByIdAsync(request.ObjectId.Value);
+        if (dbTeam == null) throw new EntityNotFoundException($"Team with ID {request.ObjectId} not found");
+
+        dbTeam.Name = request.Data.Name;
+        dbTeam.Description = request.Data.Description;
+        dbTeam.UpdatedAt = DateTime.UtcNow;
+        dbTeam.UpdatedBy = request.UserId;
+
+        await _unitOfWork.SaveChangesAsync();
+
+        return new TeamResponse(dbTeam.Id, dbTeam.Name, dbTeam.Description, dbTeam.TeamMemberships.Count);
     }
 }
