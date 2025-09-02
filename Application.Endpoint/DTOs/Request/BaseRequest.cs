179d8f63f8b7f48f9e390ff475285f002ea83596
DlopezS98 <01dlopezs98@gmail.com>
2025-09-01 21:10:32 -0600

    Add base request DTO for non-generic data

--- Application.Endpoint/DTOs/Request/BaseRequest.cs
+++ Application.Endpoint/DTOs/Request/BaseRequest.cs
@@ -1,3 +1,4 @@
 namespace DevStack.Application.BoardForge.DTOs.Request;
 
 public record BaseRequest<TData>(int? ObjectId, int UserId, TData Data);
+public record BaseRequest(int ObjectId, int UserId);
