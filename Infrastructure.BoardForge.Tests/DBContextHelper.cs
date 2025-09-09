853785f4478b8a979ef782d63a886c48df2a68ba
DlopezS98 <01dlopezs98@gmail.com>
2025-09-09 17:21:32 -0600

    Add DBContextHelper class for in-memory database context creation in tests

---
+++ Infrastructure.BoardForge.Tests/DBContextHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using DevStack.Infrastructure.BoardForge.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevStack.Infrastructure.BoardForge.Tests;
+
+[ExcludeFromCodeCoverage]
+public static class DBContextHelper
+{
+    public static BoardForgeDbContext CreateContext(Action<BoardForgeDbContext>? setup, string databaseName = "BoardForgeDBTest")
+    {
+        var options = new DbContextOptionsBuilder<BoardForgeDbContext>()
+            .UseInMemoryDatabase(databaseName)
+            .Options;
+        if (setup is not null)
+        {
+            var context = new BoardForgeDbContext(options);
+            setup(context);
+            context.SaveChanges();
+        }
+
+        return new BoardForgeDbContext(options);
+    }
+
+}
