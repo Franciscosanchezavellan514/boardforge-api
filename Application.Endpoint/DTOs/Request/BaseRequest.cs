b914f5fc85e5b5af2496dd42cfd5bee41ad65b8c
DlopezS98 <01dlopezs98@gmail.com>
2025-09-14 20:34:57 -0600

    Enhance documentation for request records in BaseRequest.cs

--- Application.Endpoint/DTOs/Request/BaseRequest.cs
+++ Application.Endpoint/DTOs/Request/BaseRequest.cs
@@ -1,5 +1,23 @@
 namespace DevStack.Application.BoardForge.DTOs.Request;
 
+/// <summary>
+/// Generic request wrapper for create operations
+/// </summary>
+/// <typeparam name="TData">The type of the data being wrapped</typeparam>
+/// <param name="UserId">The ID of the user making the request</param>
+/// <param name="Data">The data being wrapped</param>
 public record CreateRequest<TData>(int UserId, TData Data);
+/// <summary>
+/// Generic request wrapper for operations that require the root object ID and user ID
+/// </summary>
+/// <typeparam name="TData">The type of the data being wrapped</typeparam>
+/// <param name="ObjectId">The ID of the root object (e.g., team ID)</param>
+/// <param name="UserId">The ID of the user making the request</param>
+/// <param name="Data">The data being wrapped</param>
 public record BaseRequest<TData>(int ObjectId, int UserId, TData Data);
+/// <summary>
+/// Generic request wrapper for operations that only require the root object ID and user ID
+/// </summary>
+/// <param name="ObjectId">The ID of the root object (e.g., team ID)</param>
+/// <param name="UserId">The ID of the user making the request</param>
 public record BaseRequest(int ObjectId, int UserId);
