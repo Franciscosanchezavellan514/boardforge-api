7c6edeaf37b5f6f803a872cef06192a925514606
DlopezS98 <01dlopezs98@gmail.com>
2025-09-14 20:59:27 -0600

    Add UpdateTeamMembershipRequest record for managing team member roles

--- Application.Endpoint/DTOs/Request/TeamRequest.cs
+++ Application.Endpoint/DTOs/Request/TeamRequest.cs
@@ -5,6 +5,7 @@ namespace DevStack.Application.BoardForge.DTOs.Request;
 public record CreateTeamRequest([Required] string Name, string Description);
 public record UpdateTeamRequest([Required] string Name, string Description);
 public record AddTeamMemberRequest([Required] int UserId, [Required] string Role);
+public record UpdateTeamMembershipRequest([Required] string Role);
 public record RemoveTeamMemberRequest([Required] int UserId);
 public record AddTeamLabelRequest([Required] string Name, string? Color);
 public record UpdateTeamLabelRequest([Required] string Name, string? Color);
