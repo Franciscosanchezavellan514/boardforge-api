919606f518289b04bbb37c6f83602a4ba3da6663
DlopezS98 <01dlopezs98@gmail.com>
2025-09-16 12:15:23 -0600

    Remove ETag column from Cards table and update model snapshot

---
+++ Infrastructure.BoardForge/Data/Migrations/20250916181427_RemoveEtagFieldFromLabelEntity.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevStack.Infrastructure.BoardForge.Data.Migrations
+{
+    /// <inheritdoc />
+    [ExcludeFromCodeCoverage]
+    public partial class RemoveEtagFieldFromLabelEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ETag",
+                table: "Cards");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ETag",
+                table: "Cards",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: true);
+        }
+    }
+}
