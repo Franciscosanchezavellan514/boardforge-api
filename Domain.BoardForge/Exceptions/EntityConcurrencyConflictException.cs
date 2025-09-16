249d1c4a9f31cc7140e421385867f9dbf6c767b6
DlopezS98 <01dlopezs98@gmail.com>
2025-09-16 11:51:07 -0600

    Add EntityConcurrencyConflictException for handling concurrency conflicts in entities

---
+++ Domain.BoardForge/Exceptions/EntityConcurrencyConflictException.cs
@@ -0,0 +1,27 @@
+using DevStack.Domain.BoardForge.Entities;
+
+namespace DevStack.Domain.BoardForge.Exceptions;
+
+public class EntityConcurrencyConflictException<TEntity> : Exception where TEntity : BaseEntity
+{
+    private static readonly string DefaultMessage = $"Concurrency conflict occurred for entity {typeof(TEntity).Name}. The entity may have been modified or deleted by another process.";
+    TEntity? Entity { get; }
+
+    public EntityConcurrencyConflictException() : base(DefaultMessage)
+    {
+    }
+
+    public EntityConcurrencyConflictException(string message) : base(message)
+    {
+    }
+
+    public EntityConcurrencyConflictException(string message, TEntity entity) : base(message)
+    {
+        Entity = entity;
+    }
+
+    public EntityConcurrencyConflictException(TEntity entity) : base(DefaultMessage)
+    {
+        Entity = entity;
+    }
+}
