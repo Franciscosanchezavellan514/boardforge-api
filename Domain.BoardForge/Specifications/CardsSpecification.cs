d997df1211bc78e685cf830e2f82fb6947d8b2e5
DlopezS98 <01dlopezs98@gmail.com>
2025-09-19 01:21:40 -0600

    Remove CardsByTeamIdSpecification class to streamline specifications

--- Domain.BoardForge/Specifications/CardsSpecification.cs
+++ Domain.BoardForge/Specifications/CardsSpecification.cs
@@ -2,17 +2,6 @@ using DevStack.Domain.BoardForge.Entities;
 
 namespace DevStack.Domain.BoardForge.Specifications;
 
-public class CardsByTeamIdSpecification : BaseSpecification<Card>
-{
-    public CardsByTeamIdSpecification(int teamId)
-    {
-        ApplyCriteria(c => c.TeamId == teamId && c.IsActive);
-        ApplyOrderBy(o => o.Title);
-        ApplyOrderBy(o => o.BoardId);
-        AddInclude(i => i.Labels);
-    }
-}
-
 public class CardByIdAndTeamIdSpecification : BaseSpecification<Card>
 {
     public CardByIdAndTeamIdSpecification(int id, int teamId)
