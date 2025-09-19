8f310a4924f6aeef0d7ab9df46ab272d2642a365
DlopezS98 <01dlopezs98@gmail.com>
2025-09-18 20:47:47 -0600

    Remove TeamResource model as it is no longer needed

--- BoardForgeAPI/Models/TeamResource.cs
+++
@@ -1,9 +0,0 @@
-using DevStack.Domain.BoardForge.Entities;
-
-namespace DevStack.BoardForgeAPI.Models;
-
-public record TeamResource(int TeamId) : ITeamResource
-{
-    public int TeamId { get; set; } = TeamId;
-    public Team? Team { get; set; }
-}
